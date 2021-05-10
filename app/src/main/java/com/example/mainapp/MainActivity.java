ee595ce5a3c85d083d6963fcf3636c620828adab
kld9223 <kld9223@gmail.com>
2021-05-10 16:28:34 +0900

    안녕

--- app/src/main/java/com/example/mainapp/MainActivity.java
+++ app/src/main/java/com/example/mainapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//안녕
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
